4aaef027141522c5157f19118fc2c88b889d6b2f
Lucas Lehm <lucaslehm@outlook.com>
2026-04-12 01:23:10 -0300

    Update Exercicio02.java

--- aula_02/Exercicio02.java
+++ aula_02/Exercicio02.java
@@ -2,9 +2,10 @@ package aula_02;
 
 public class Exercicio02 {
     public static void main(String[] args) {
-        // Crie um programa que realize a média de duas notas decimais e exiba o resultado.
+        // Crie um programa que realize a média de duas notas decimais e exiba o
+        // resultado.
         double notaMatematica = 10.0;
-        double notaPortugues =  7.1;
+        double notaPortugues = 7.1;
 
         System.out.println("A média do Aluno é: " + ((notaMatematica + notaPortugues) / 2));
     }
